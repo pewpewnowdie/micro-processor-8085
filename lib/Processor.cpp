af352bf58c73e74d13ee2daeb1b4ede8ec69b037
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2024-02-14 09:34:56 +0530

    Update Processor.cpp

--- lib/Processor.cpp
+++ lib/Processor.cpp
@@ -40,10 +40,10 @@ class MemoryMapper {
 };
 
 class Processor {
-    uint8_t A,B,C,D,E,H,L;
-    uint8_t SP,PC;
-    bool flag[8];
     public:
+        uint8_t A,B,C,D,E,H,L;
+        uint8_t SP,PC;
+        bool flag[8];
         MemoryMapper memory;
         Processor() {
             A=0;
@@ -58,26 +58,8 @@ class Processor {
             for(int i=0;i<8;i++)
                 flag[i]=false;
         }
-        void setA(uint8_t a) {A=a;}
-        void setB(uint8_t b) {B=b;}
-        void setC(uint8_t c) {C=c;}
-        void setD(uint8_t d) {D=d;}
-        void setE(uint8_t e) {E=e;}
-        void setH(uint8_t h) {H=h;}
-        void setL(uint8_t l) {L=l;}
-        void setSP(uint8_t sp) {SP=sp;}
-        void setPC(uint8_t pc) {PC=pc;}
         void setFlag(int f) {flag[f]=true;}
         void resetFlag(int f) {flag[f]=false;}
-        uint8_t getA() {return A;}
-        uint8_t getB() {return B;}
-        uint8_t getC() {return C;}
-        uint8_t getD() {return D;}
-        uint8_t getE() {return E;}
-        uint8_t getH() {return H;}
-        uint8_t getL() {return L;}
-        uint8_t getSP() {return SP;}
-        uint8_t getPC() {return PC;}
         bool getFlag(int f) {return flag[f];}
         void showReg() {
             cout<<"A: "<< hex << unsigned(A)<<endl;
